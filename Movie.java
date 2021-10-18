da4eb9f9485b1a55c564fb60d0ee9155a00e6711
Cassidy Barr <90427280+Jinx2498@users.noreply.github.com>
2021-10-17 22:54:45 -0400

    Create Movie.java

---
+++ Movie.java
@@ -0,0 +1,36 @@
+import java.time.LocalDate;
+
+public class Movie extends Item {
+
+    private String director;
+    private String producer;
+
+    public Movie(String title, double regularPrice, LocalDate releaseDate, int quantity, String director, String producer) {
+
+        super(title, regularPrice, releaseDate, quantity);
+        this.director = director;
+        this.producer = producer;
+        
+    }
+
+    public Movie(String title, double regularPrice, LocalDate releaseDate, int quantity, String director, String producer, int itemID) {
+
+        super(title, regularPrice, releaseDate, quantity);
+        this.director = director;
+        this.producer = producer;
+        
+    }
+
+    @Override
+    public String toString() {
+
+        return super.toString() + "\n\tDirector: \n" + this.director + "\n\tProducer: \n" + this.producer; 
+
+    }
+
+    @Override
+    public Item makeCopy() {
+
+    }
+
+}
