66d8569dd477eef26ce9298ae840d76fd772da41
Cassidy Barr <90427280+Jinx2498@users.noreply.github.com>
2021-10-18 22:12:32 -0400

    fixed make copy

--- CompactDisk.java
+++ CompactDisk.java
@@ -1,21 +1,21 @@
 import java.time.LocalDate;
 
-public class CompactDisk extends Item{
+public class CompactDisc extends Item{
 
     private String bandName;
     private String recordLabel;
 
-    public CompactDisk(String title, double regularPrice, LocalDate releaseDate, int quantity, String bandName, String recordLabel) {
+    public CompactDisc(String title, double regularPrice, LocalDate releaseDate, int quantity, String bandName, String recordLabel) {
         super(title, regularPrice, releaseDate, quantity);
         this.bandName = bandName;
         this.recordLabel = recordLabel;
     }
 
-    public CompactDisk(String title, double regularPrice, LocalDate releaseDate, int quantity, String bandName) {
+    public CompactDisc(String title, double regularPrice, LocalDate releaseDate, int quantity, String bandName) {
         this(title, regularPrice, releaseDate, quantity, bandName, "Indie");
     }
 
-    public CompactDisk(String title, double regularPrice, LocalDate releaseDate, int quantity, String bandName, String recordLabel, int itemID) {
+    public CompactDisc(String title, double regularPrice, LocalDate releaseDate, int quantity, String bandName, String recordLabel, int itemID) {
         super(title, regularPrice, releaseDate, quantity, itemID);
         this.bandName = bandName;
         this.recordLabel = recordLabel;
@@ -27,7 +27,8 @@ public class CompactDisk extends Item{
     }
 
     @Override
-    public CompactDisk makeCopy() {
-        return new CompactDisk(getTitle(), getRegularPrice(),getReleaseDate(),getQuantity(), bandName, recordLabel, getItemID());
+    public Item makeCopy() {
+        Item disc = new CompactDisc(getTitle(), getRegularPrice(),getReleaseDate(),getQuantity(), bandName, recordLabel, getItemID());
+        return disc;
     }
 }
