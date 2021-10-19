aeb83bd1a6aab0636d569bcd76ed9d1126013610
hassanuahmad <hassanusaidahmad@gmail.com>
2021-10-18 21:02:35 -0400

    Finished CompactDisk.java

--- CompactDisk.java
+++ CompactDisk.java
@@ -6,41 +6,29 @@ public class CompactDisk extends Item{
     private String recordLabel;
 
     public CompactDisk(String title, double regularPrice, LocalDate releaseDate, int quantity, String bandName, String recordLabel) {
-
         super(title, regularPrice, releaseDate, quantity);
         this.bandName = bandName;
         this.recordLabel = recordLabel;
-        
     }
 
-    // public CompactDisk(String bandName) {
-
-
-        
-    // }
+    public CompactDisk(String title, double regularPrice, LocalDate releaseDate, int quantity, String bandName) {
+        this(title, regularPrice, releaseDate, quantity, bandName, "Indie");
+    }
 
     public CompactDisk(String title, double regularPrice, LocalDate releaseDate, int quantity, String bandName, String recordLabel, int itemID) {
-
         super(title, regularPrice, releaseDate, quantity, itemID);
         this.bandName = bandName;
         this.recordLabel = recordLabel;
-        
     }
 
     @Override
     public String toString() {
-
-        return super.toString() + "\n\tBand: " + this.bandName + "\n\tBand: " + this.recordLabel;
-
+        return super.toString() + "\nBand:\t" + bandName + "\nRecord Label:\t" + recordLabel;
     }
 
     @Override
     public Item makeCopy(Item item) {
-
         Item copy = new Item(item.getTitle(), item.getRegularPrice(), item.getReleaseDate(), item.getQuantity(), item.getItemID());
-
         return copy;
-
     }
-    
 }
